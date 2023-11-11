8d6b1e85b2f4964453d30c577b8bbe823925e60d
Tannon Kew <kew@cl.uzh.ch>
2023-11-11 12:41:33 +0100

    upgrade to openai==1.2.2

---
+++ api_request_parallel_processor.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+This script can be used to make parallel requests to the OpenAI API.
+
+It is based on the following blog post:
+    https://towardsdatascience.com/the-proper-way-to-make-calls-to-chatgpt-api-52e635bea8ff
+
+"""
+
+import asyncio
+import aiohttp
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
+
+from helpers import logger
+from api_secrets import OPENAI_API_KEY
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {OPENAI_API_KEY}"
+}
+
+max_retries = 20
+
+costings = {
+        "gpt-3.5-turbo": (0.0015, 0.0020), 
+        "gpt-3.5-turbo-16k": (0.003, 0.004), 
+        "gpt-4": (0.03, 0.06), # 8k context
+        "gpt-3.5-turbo-1106": (0.0010, 0.0020),
+    }
+
+def get_cost(model_name, prompt_tokens, completion_tokens):
+    prompt_cost = ((prompt_tokens / 1000) * costings[model_name][0])
+    completion_cost = ((completion_tokens / 1000) * costings[model_name][1])
+    return prompt_cost + completion_cost
+
+class ProgressLog:
+    def __init__(self, total, model_name):
+        self.total = total
+        self.model_name = model_name
+        self.done = 0
+        self.prompt_tokens = 0
+        self.completion_tokens = 0
+        self.cost = 0
+
+    def increment(self, prompt_tokens, completion_tokens, cost):
+        self.done = self.done + 1
+        self.prompt_tokens += prompt_tokens
+        self.completion_tokens += completion_tokens
+        self.cost += cost
+
+    def __repr__(self):
+        return f"Completed runs {self.done}/{self.total}, total prompt tokens: {self.prompt_tokens}, total completion tokens: {self.completion_tokens}, total cost: {self.cost:.8f}"
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(max_retries), before_sleep=logger.info, retry_error_callback=lambda _: None)
+async def get_completion(model_name, messages, session, semaphore, progress_log, seed, temperature, expects_json):
+    async with semaphore:
+
+        async with session.post("https://api.openai.com/v1/chat/completions", headers=headers, json={
+            "model": model_name,
+            "messages": messages,
+            "temperature": temperature,
+            "seed": seed,
+            "response_format": { "type": "json_object" } if expects_json else None,
+        }) as resp:
+
+            response_json = await resp.json()
+            
+            if response_json.get("error") is not None:
+                logger.warning(f"{response_json['error']['type']}: {response_json['error']['message']}")
+
+            cost = get_cost(model_name, response_json["usage"]["prompt_tokens"], response_json["usage"]["completion_tokens"])
+            progress_log.increment(response_json["usage"]["prompt_tokens"], response_json["usage"]["completion_tokens"], cost)
+
+            response_dict = {
+                "content": response_json["choices"][0]["message"]["content"],
+                "system_fingerprint": response_json["system_fingerprint"],
+                "prompt_tokens": response_json["usage"]["prompt_tokens"],
+                "completion_tokens": response_json["usage"]["completion_tokens"],
+                "cost": cost,
+                "model_name": model_name,
+            }
+
+            logger.info(progress_log)
+            
+            return response_dict
+
+async def get_completion_list(model_name, content_list, max_parallel_calls, timeout, seed, temperature, expects_json):
+    semaphore = asyncio.Semaphore(value=max_parallel_calls) # manages the amount of async calls that are currently being performed within its context
+    progress_log = ProgressLog(len(content_list), model_name)
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(timeout)) as session:
+        return await asyncio.gather(*[get_completion(model_name=model_name, messages=content, session=session, semaphore=semaphore, progress_log=progress_log, seed=seed, temperature=temperature, expects_json=expects_json) for content in content_list])
+
+async def _run_api_request_parallel_process(model_name, content_list, max_parallel_calls, timeout, seed, temperature, expects_json):
+    import time
+    start_time = time.perf_counter()
+
+    completion_list = await get_completion_list(model_name=model_name, content_list=content_list, max_parallel_calls=max_parallel_calls, timeout=timeout, seed=seed, temperature=temperature, expects_json=expects_json)
+    print("Time elapsed: ", time.perf_counter() - start_time, "seconds.")
+    print("Total cost: ", sum([completion["cost"] for completion in completion_list if completion is not None]))
+    return completion_list
+    
+# main function to be called from outside
+def run_api_request_parallel_process(model_name, content_list, max_parallel_calls=5, timeout=60, seed=42, temperature=0.7, expects_json=False):
+    return asyncio.run(_run_api_request_parallel_process(model_name, content_list, max_parallel_calls=max_parallel_calls, timeout=timeout, seed=seed, temperature=temperature, expects_json=expects_json))
+
+if __name__ == "__main__":
+
+    def prepare_messages(prompts):
+        return [[{"role": "user", "content": prompt}] for prompt in prompts]
+    prompts = [f"repeat the following number once. {i}" for i in range(5)]
+    prompts = prepare_messages(prompts)
+    
+    # should work
+    run_api_request_parallel_process(model_name="gpt-3.5-turbo-1106", content_list=prompts, max_parallel_calls=2, timeout=10, seed=42, temperature=0.0, expects_json=False)
+
+    # should fail with warnings logged
+    run_api_request_parallel_process(model_name="gpt-3.5-turbo-1106", content_list=prompts, max_parallel_calls=2, timeout=10, seed=42, temperature=0.0, expects_json=True)
--- translate_with_gpt.py
+++ translate_with_gpt.py
@@ -109,13 +109,23 @@ Example call:
         --dataset_type "lima" \
         --model_name "gpt-3.5-turbo-16k"
         
+    python translate_with_gpt.py \
+        --input_file data/outputs/llama_2_7b_hf_de_merged/alpaca_eval_instructions_de-none-guanaco_prompt-s0-k50-p0.9-t0.8-b8.jsonl \
+        --output_file alpaca_eval_instructions_de-none-guanaco_prompt-s0-k50-p0.9-t0.8-b8-translated.jsonl \
+        --tgt_lang "English" \
+        --src_key "system" \
+        --dataset_type "alpaca_eval" \
+        --model_name "gpt-3.5-turbo-1106" --original_prompts data/alpaca_eval_instructions_en.json
 """
 
+import sys
 import json
 import argparse
 import pandas as pd
 from typing import Dict, List
 from tqdm import tqdm
+import math
+from pathlib import Path
 
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
@@ -124,10 +134,11 @@ from langchain.schema import (
     SystemMessage
 )
 from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
-from langchain import LLMChain
+from langchain.chains import LLMChain
 from langchain.callbacks import get_openai_callback
 import tiktoken
 
+from helpers import logger
 from api_secrets import OPENAI_API_KEY
 
 system_prompt = "You are a helpful assistant."
@@ -166,11 +177,12 @@ Original:
 
 Translation into {tgt_lang}:"""
 
-
+# costings per model in USD for 1k input and output tokens
 costings = {
-    "gpt-3.5-turbo": 0.002, # $0.002 per output token (input tokens are slightly cheaper)
-    "gpt-3.5-turbo-16k": 0.004,
-    "gpt-4": 0.06, # 8k context
+    "gpt-3.5-turbo": (0.0015, 0.0020), 
+    "gpt-3.5-turbo-16k": (0.003, 0.004), 
+    "gpt-4": (0.03, 0.06), # 8k context
+    "gpt-3.5-turbo-1106": (0.0010, 0.0020),
 }
 
 
@@ -180,11 +192,15 @@ def set_args():
     ap.add_argument("--output_file", type=str, required=False, default=None)
     ap.add_argument("--model_name", type=str, default="gpt-3.5-turbo")
     ap.add_argument("--src_key", type=str, default="instruction")
-    ap.add_argument("--tgt_lang", type=str, default="de")
+    ap.add_argument("--tgt_lang", type=str, default="German")
     ap.add_argument("--debug", action="store_true")
     ap.add_argument("--inspect_only", action="store_true")
     ap.add_argument("--max_tokens", type=int, default=2048)
     ap.add_argument("--dataset_type", type=str, default="alpaca_eval")
+    ap.add_argument("--limit", type=int, default=None)
+    ap.add_argument("--seed", type=int, default=42)
+    ap.add_argument("--force", action="store_true", help="Overwrite output file if it already exists")
+    ap.add_argument("--original_prompts", default=None, type=str, help="Path to original prompts file if required, e.g. for Alpaca Eval: data/alpaca_eval_instructions_en.json")
     return ap.parse_args()
 
 def prepare_prompt(prompt, tgt_lang, human_template, system_prompt):
@@ -194,18 +210,20 @@ def prepare_prompt(prompt, tgt_lang, human_template, system_prompt):
 
     return [system_message, human_message]
 
-def run_llm(prompts, model_name="gpt-3.5-turbo", max_tokens=1000):
+def run_llm(prompts, model_name="gpt-3.5-turbo-1106", max_tokens=1000):
     """Get a judgement from the LLM judge."""
     
     total_cost = 0
     total_tokens = 0
+    total_prompt_tokens = 0
+    total_completion_tokens = 0
 
     model = ChatOpenAI(
         model_name=model_name,
-        temperature=0.01,
+        temperature=0.0,
         max_tokens=max_tokens,
         openai_api_key=OPENAI_API_KEY,
-        request_timeout=60,
+        # request_timeout=60,
         )
 
     responses = []
@@ -213,13 +231,23 @@ def run_llm(prompts, model_name="gpt-3.5-turbo", max_tokens=1000):
         with get_openai_callback() as cb:
             response = model.generate([prompt])
             total_cost += cb.total_cost
+            total_prompt_tokens += cb.prompt_tokens
+            total_completion_tokens += cb.completion_tokens
             total_tokens += cb.total_tokens
             yield response.generations[0][0].text
 
     print(f"Model: {model_name}")
     print(f"# of prompts: {len(prompts)}")
     print(f"Total cost: {total_cost}")
+    print(f"Total prompt tokens: {total_prompt_tokens}")
+    print(f"Total completion tokens: {total_completion_tokens}")
     print(f"Total tokens: {total_tokens}")
+    # calculate total cost using costings dict since langchain doesn't include the cost of most recent models
+    prompt_token_costs = total_prompt_tokens/1000 * costings[model_name][0]
+    completion_token_costs = total_completion_tokens/1000 * costings[model_name][1]
+    print(f"Cost for prompt tokens: {prompt_token_costs:.4f}")
+    print(f"Cost for completion tokens: {completion_token_costs:.4f}")
+    print(f"Total cost: {prompt_token_costs + completion_token_costs:.4f}")
     
         
 if __name__ == "__main__":
@@ -227,46 +255,83 @@ if __name__ == "__main__":
     args = set_args()
 
     data = pd.read_json(args.input_file, lines=True)
-
-    instructions = data[args.src_key].tolist()
     
-    if args.dataset_type == "alpaca_eval":
+    # infer output file name if not specified - only works for Alpaca Eval outputs
+    output_file = args.output_file
+    if not output_file:
+        if args.dataset_type == "alpaca_eval_outputs":
+            ext = Path(args.input_file).suffix
+            output_file = args.input_file.replace(ext, f"-with_{args.tgt_lang[:2].lower()}{ext}")
+            logger.info(f"Inferred output file name: {output_file}")
+        else:
+            raise ValueError("Please specify an output file name. We currently only infer output file names for --dataset_type==\"alpaca_eval_outputs\"")
+
+    if Path(output_file).exists() and not args.force:
+        raise ValueError(f"Output file {output_file} already exists. Please specify a different output file name or use --force to overwrite.")
+    
+    if args.dataset_type == "alpaca_eval_outputs":
+        if args.original_prompts:
+            # get the original prompts as well
+            en_prompts = pd.read_json(args.original_prompts, lines=True).rename(columns={"instruction": "source_en"})
+            # merge the original prompts with the data
+            data = pd.concat([data, en_prompts], axis=1)
+        else:
+            raise ValueError("Please specify the path to the original prompts file with --original_prompts")
+
+        if args.limit:
+            # sample the data up to the limit
+            data['id'] = data.index # persist original index
+            data = data.sample(n=min(args.limit, len(data)), random_state=args.seed).reset_index(drop=True)
+
+    data = data.to_dict(orient="records")
+    
+    instructions = [item[args.src_key] for item in data]
+    if args.dataset_type == "alpaca_eval": # for translating Alpaca Eval instructions from English to other languages
         prompts = [prepare_prompt(i, args.tgt_lang, human_template, system_prompt) for i in instructions]
+    
+    elif args.dataset_type == "alpaca_eval_outputs": # for translating non-English outputs from Alpaca Eval to English
+        prompts = [prepare_prompt(i, args.tgt_lang, human_template, system_prompt) for i in instructions]
+
     elif args.dataset_type == "lima":
         # replace original role labels with <S1> and <S2> for better translation
         instructions = [i.replace("### Human:", "<S1>").replace("### Assistant:", "<S2>") for i in instructions]
         prompts = [prepare_prompt(i, args.tgt_lang, lima_human_template, system_prompt) for i in instructions]
+    
     elif args.dataset_type == "guanaco":
         # replace original role labels with <S1> and <S2> for better translation
         instructions = [i.replace("### Human:", "<S1>").replace("### Assistant:", "<S2>") for i in instructions]
         prompts = [prepare_prompt(i, args.tgt_lang, guanaco_human_template, system_prompt) for i in instructions]
+    
     else:
         raise ValueError(f"Unknown dataset type: {args.dataset_type}")
 
     # estimate number of tokens
     tokenizer = tiktoken.encoding_for_model(args.model_name)
     prompt_lengths = [len(tokenizer.encode(prompt[0].content + '\n\n' + prompt[1].content)) for prompt in prompts]
-    print(f"Total tokens: {sum(prompt_lengths)}")
-    print(f'Average tokens per prompt: {sum(prompt_lengths)/len(prompt_lengths)}')
-    print(f'Max tokens per prompt: {max(prompt_lengths)}')
-    print(f'Min tokens per prompt: {min(prompt_lengths)}')
-    print(f'Rough estimated cost: {((sum(prompt_lengths) * 2) / 1000) * costings[args.model_name]}')
+    logger.info(f"Total tokens: {sum(prompt_lengths)}")
+    logger.info(f'Average tokens per prompt: {sum(prompt_lengths)/len(prompt_lengths)}')
+    logger.info(f'Max tokens per prompt: {max(prompt_lengths)}')
+    logger.info(f'Min tokens per prompt: {min(prompt_lengths)}')
+    logger.info(f'Rough estimated cost: {((sum(prompt_lengths) * 2) / 1000) * costings[args.model_name][1]}')
 
     if args.inspect_only:
-        exit()
+        sys.exit()
 
     if args.debug:
         prompts = prompts[:5]
 
-    print(f"Running LLM {args.model_name} on {len(prompts)} prompts ...")
+    logger.info(f"Running LLM {args.model_name} on {len(prompts)} prompts ...")
 
-    print(f"Example prompt: {prompts[0]}")
+    logger.info(f"Example prompt: {prompts[0]}")
 
     c = 0
-    with open(args.output_file, "w", encoding='utf8') as f:
-        for translation in run_llm(prompts=prompts, model_name=args.model_name, max_tokens=args.max_tokens):
-            c += 1
-            line = {args.src_key: translation}
+    with open(output_file, "w", encoding='utf8') as f:
+        for line, translation in zip(data, run_llm(prompts=prompts, model_name=args.model_name, max_tokens=args.max_tokens)):
+            if args.dataset_type == "alpaca_eval_outputs":
+                line[f'{args.src_key}_{args.tgt_lang[:2].lower()}_{args.model_name}'] = translation
+            else:
+                line = {args.src_key: translation}
             f.write(f"{json.dumps(line, ensure_ascii=False)}\n")
+            c += 1
 
-    print(f"Wrote {c} translations to {args.output_file}")
+    logger.info(f"Wrote {c} translations to {output_file}")
