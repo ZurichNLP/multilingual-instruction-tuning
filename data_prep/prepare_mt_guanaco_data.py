4228ba069fc684a9ab50fc2c453122a62377de81
Tannon Kew <kew@cl.uzh.ch>
2024-01-09 09:18:49 +0100

    incremental examples per language

--- data_prep/prepare_mt_guanaco_data.py
+++ data_prep/prepare_mt_guanaco_data.py
@@ -3,14 +3,19 @@
 
 """
 
-Gathers translated guanaco data into a single dataframe to construct a multilingual training set.
-
+This script creates multiple training sets with 1 to 6 languages.
+The script will subsample n samples for each non-English language from the translated data subsets.
+By default, n=200. For other values, pass the desired n as an argument to the script.
 
 Example usage:
     python prepare_mt_guanaco_data.py
 
+    # for other values of n, e.g. 10 use:
+    python prepare_mt_guanaco_data.py 10
+
 """
 
+import sys
 from pathlib import Path
 import pandas as pd
 
@@ -18,10 +23,6 @@ seed = 42
 n = 200
 data_dir = 'data/guanaco'
 
-mono_en = pd.read_json(Path(data_dir) / 'guanaco_train_mono.json', lines=True)
-mono_en.info()
-
-# tgt_langs = ['en', 'ca', 'de', 'es', 'fr', 'ru', 'zh']
 tgt_langs = [
     'en', # 3627
     'es', # 3850
@@ -32,28 +33,14 @@ tgt_langs = [
     'ca', # 250
 ]
 
+if len(sys.argv) > 1:
+    n = int(sys.argv[1])
+print(f'Using {n} samples per language')
 
+# 1. load the full EN monolingual guanaco data
+mono_en = pd.read_json(Path(data_dir) / 'guanaco_train_mono.json', lines=True)
+mono_en.info()
 mono_ids = set(mono_en['id'].tolist())
-diff_ids = None
-for lang in tgt_langs[1:]:
-    ml6 = pd.read_json(Path(data_dir) f'guanaco_train_{lang}.json', lines=True)
-    ml6 = ml6[ml6['lang'] == 'en']
-    common_ids = mono_ids.intersection(ml6['id'].tolist())
-    # get ids not in common
-    diffs = mono_ids.difference(set(ml6['id'].tolist()))
-    if diff_ids is None:
-        diff_ids = diffs
-    elif diffs != diff_ids:
-        print('WARNING: diffs not the same!')
-    
-    print(lang, len(common_ids), len(diff_ids))
-
-# get the rows from mono_en that are not in common_ids for translation into other langs
-to_translate_df = mono_en[mono_en['id'].isin(diffs)]
-to_translate_df.info()
-
-non_translated_df = mono_en[mono_en['id'].isin(common_ids)] # mono_en[~mono_en['id'].isin(diffs)]
-non_translated_df.info()
 
 def replace_role_labels(text):
     text = text.replace('<S1>', '### Human:')
@@ -80,16 +67,25 @@ non_eng_langs = [lang for lang in tgt_langs if lang != 'en']
 # shuffle data
 translated_df = translated_df.sample(frac=1, random_state=seed).reset_index(drop=True)
 
+# if n is less than 200, subsample the data to the total number of non-eng samples
+if n < 200:
+    translated_df = translated_df[:n * len(non_eng_langs)]
+
 translated_df['text'] = None
 lang_i = 0
+
 for i, row in translated_df.iterrows():
     
-    if i > 0 and i % 200 == 0:
+    if i > 0 and i % n == 0:
         lang_i += 1
     
     translated_df.at[i, 'text'] = row[non_eng_langs[lang_i]]
     translated_df.at[i, 'lang'] = non_eng_langs[lang_i]
 
+translated_ids = set(translated_df['id'].tolist())
+
+# remove translated samples from mono_en
+non_translated_df = mono_en[~mono_en['id'].isin(translated_ids)].reset_index(drop=True)
 
 ids = None
 for i, lang in enumerate(non_eng_langs):
@@ -98,6 +94,8 @@ for i, lang in enumerate(non_eng_langs):
     non_en_data_to_add = translated_df[translated_df['lang'].isin(non_eng_langs[:i])][['text', 'lang', 'id']]
 
     en_data_to_add = translated_df[~translated_df['id'].isin(non_en_data_to_add['id'].tolist())][['en', 'id']].rename(columns={'en': 'text'})
+    print(len(en_data_to_add))
+
     en_data_to_add['lang'] = 'en'
 
     print(f'adding {len(en_data_to_add)} rows of en data')
@@ -107,6 +105,9 @@ for i, lang in enumerate(non_eng_langs):
     # concatenate mono_dataset and added_data
     ml_df = pd.concat([non_translated_df, en_data_to_add, non_en_data_to_add]).reset_index(drop=True)
 
+    print(ml_df['lang'].value_counts())
+
+    # breakpoint()
     # ensure ids are unique
     assert len(ml_df['id'].unique()) == len(ml_df)
     
@@ -118,70 +119,14 @@ for i, lang in enumerate(non_eng_langs):
 
     print(ml_df['lang'].value_counts())
 
-    outfile = Path(data_dir) / f'guanaco_train_mt_ml{i+1}.json'
-    ml_df.to_json(outfile, orient='records', lines=True, force_ascii=False)
-    print(f'Wrote {len(ml_df)} samples to {outfile}')
-
-# 3. create ml LOO datasets
-for i, lang in enumerate(non_eng_langs):
+    if n == 200:
+        outfile = Path(data_dir) / f'guanaco_train_mt_ml{i+1}.json'
+    # ablation study train sets - only for ml3
+    elif n < 200 and i == 2:
+        outfile = Path(data_dir) / f'guanaco_train_mt_ml{i+1}_n{n}.json'
+    else:
+        outfile = None
     
-    other_langs = non_eng_langs[:i] + non_eng_langs[i+1:]
-    print(lang, other_langs)
-
-    c = 0
-    # for each other lang, take n non-overlapping rows
-    sdfs = []
-    for other_lang in other_langs:
-        sdf = translated_df.iloc[c:c+n][['id', other_lang]]
-        sdf['lang'] = other_lang
-        sdf.rename(columns={other_lang: 'text'}, inplace=True)
-        print(len(sdf), sdf.columns)
-        sdfs.append(sdf)
-        c += n
-
-    ml_df = pd.concat(sdfs + [non_translated_df], axis=0).reset_index(drop=True)
-    
-    # shuffle
-    ml_df = ml_df.sample(frac=1, random_state=seed+1).reset_index(drop=True)
-
-    # ensure ids are unique
-    assert len(ml_df['id'].unique()) == len(ml_df)
-
-    ml_df.to_json(Path(data_dir) / f'guanaco_train_mt_{lang}.json', orient='records', lines=True, force_ascii=False)
-    print(f'wrote {len(ml_df)} rows to guanaco_train_mt_{lang}.json')
-  
-# # 3. create ml LOO datasets with overlapping ids - Not used
-# for i, lang in enumerate(non_eng_langs):
-    
-#     other_langs = non_eng_langs[:i] + non_eng_langs[i+1:]
-#     print(lang, other_langs)
-
-#     c = 0
-#     # for each other lang, take n non-overlapping rows
-#     sdfs = []
-#     for other_lang in other_langs:
-#         sdf = translated_df.iloc[c:c+n][['id', other_lang]]
-#         sdf['lang'] = other_lang
-#         sdf.rename(columns={other_lang: 'text'}, inplace=True)
-#         print(len(sdf), sdf.columns)
-#         sdfs.append(sdf)
-#         c += n
-    
-#     sdfs = pd.concat(sdfs, axis=0).reset_index(drop=True).sort_values(by=['id'])
-
-#     # get rows from to_translate_df that match in sdfs    
-#     x = to_translate_df[to_translate_df['id'].isin(sdfs['id'].tolist())].sort_values(by=['id'])
-
-#     # get remainder of rows from non_translated_df
-#     y = non_translated_df.sample(n=total_n - len(x) - len(sdfs), random_state=seed).reset_index(drop=True)
-
-#     ml_df = pd.concat([sdfs, x, y], axis=0).reset_index(drop=True)
-    
-#     # shuffle
-#     ml_df = ml_df.sample(frac=1, random_state=seed+1).reset_index(drop=True)
-
-#     # ensure ids overlap as we want instances of the same id in different langs
-#     assert len(ml_df['id'].unique()) != len(ml_df)
-
-#     ml_df.to_json(Path(data_dir) / f'guanaco_train_mtol_{lang}.json', orient='records', lines=True, force_ascii=False)
-#     print(f'wrote {len(ml_df)} rows to guanaco_train_mtol_{lang}.json')
+    if outfile:
+        ml_df.to_json(outfile, orient='records', lines=True, force_ascii=False)
+        print(f'Wrote {len(ml_df)} samples to {outfile}')
